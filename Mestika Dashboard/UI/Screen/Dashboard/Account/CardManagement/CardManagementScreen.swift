4f9f72ebd76441947367b16c30aef05fecae92f6
Abdul R. Arraisi <itsarraisi@gmail.com>
2020-11-05 12:01:30 +0700

    update kartuku

--- Mestika Dashboard/UI/Screen/Dashboard/Account/CardManagement/CardManagementScreen.swift
+++ Mestika Dashboard/UI/Screen/Dashboard/Account/CardManagement/CardManagementScreen.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct CardManagementScreen: View {
-    @State var data = kartuKuData
+    @State var data = [
+    Card(id: 0, imageName: "rekening-card-3", name: "KARTU 1", description: "KARTU 1", activeStatus: false, isShow: false),
+    Card(id: 1, imageName: "rekening-card-4", name: "KARTU 2", description: "KARTU 2", activeStatus: true, isShow: false),
+    Card(id: 2, imageName: "rekening-card-5", name: "KARTU 3", description: "KARTU 1", activeStatus: false, isShow: false),
+    Card(id: 3, imageName: "rekening-card-6", name: "KARTU 4", description: "KARTU 2", activeStatus: false, isShow: false)
+]
+
     @State var kartuIndex: Int = 0
     
     var body: some View {
@@ -24,7 +30,7 @@ struct CardManagementScreen: View {
                 .padding(.vertical,40)
                 
                 if !kartuKuData[kartuIndex].activeStatus {
-                    DetailKartuTidakAktifView(kartu: kartuKuData[kartuIndex])
+                    DetailKartuTidakAktifView(kartu: data[kartuIndex])
                         .shadow(color: Color(hex: "#3756DF").opacity(0.2), radius: 15, x: 0.0, y: 15.0)
                 }
                 else {
