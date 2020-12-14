899c292f8a15958af71446733d6c3e4c05b05b5a
Prima Jatnika <primajatnika271995@gmail.com>
2020-12-14 08:59:15 +0700

    Normal Route

--- Mestika Dashboard/Menu/Onboarding/Views/Register/WelcomeView.swift
+++ Mestika Dashboard/Menu/Onboarding/Views/Register/WelcomeView.swift
@@ -485,7 +485,7 @@ struct WelcomeView: View {
             .background(Color(hex: "#2334D0"))
             .cornerRadius(12)
             
-            NavigationLink(destination: TujuanPembukaanRekeningView().environmentObject(registerData), isActive: self.$isNoAtmOrRekViewActive){
+            NavigationLink(destination: NoAtmOrRekeningVerificationView(rootIsActive: .constant(false)).environmentObject(registerData), isActive: self.$isNoAtmOrRekViewActive){
                 Text("Ya, saya nasabah Bank Mestika")
                     .foregroundColor(.black)
                     .font(.custom("Montserrat-SemiBold", size: 13))
