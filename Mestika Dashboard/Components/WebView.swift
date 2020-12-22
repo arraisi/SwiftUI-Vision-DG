d3f0719f1540f028849a0b66f89747e2d67953e4
Abdul R. Arraisi <itsarraisi@gmail.com>
2020-12-22 13:54:55 +0700

    fixed issue ketentuan register

--- Mestika Dashboard/Components/WebView.swift
+++ Mestika Dashboard/Components/WebView.swift
@@ -11,19 +11,45 @@ import SwiftUI
 
 /// View created as a workaround implementation of UIWebView for SwiftUI
 struct WebView: UIViewRepresentable {
-
+    
+    @Binding var readFinished: Bool
+    
     /// String representation of the URL you want to open in the WebView.
     let urlString: String?
-
+    private let  webview = WKWebView()
+    
     func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
+        
+        webview.scrollView.delegate = context.coordinator
+        return webview
     }
-
-    func updateUIView(_ uiView: WKWebView, context: Context) {
+    
+    func updateUIView(_ webview: WKWebView, context: Context) {
         if let safeString = urlString, let url = URL(string: safeString) {
             let request = URLRequest(url)
-            uiView.load(request)
+            webview.load(request)
+        }
+        
+    }
+    
+    func makeCoordinator() -> WebView.Coordinator {
+        Coordinator(webView: self)
+    }
+    
+    class Coordinator: NSObject, UIScrollViewDelegate {
+        
+        private let parent : WebView
+        
+        init(webView : WebView) {
+            self.parent = webView
+        }
+        
+        func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            if (scrollView.contentOffset.y) > (scrollView.contentSize.height - scrollView.frame.size.height) {
+                //bottom reached
+                self.parent.readFinished = true
+            } 
         }
     }
-
+    
 }
--- Mestika Dashboard/Menu/Onboarding/Views/Register/Non Nasabah Verification/KetentuanRegisterNonNasabahView.swift
+++ Mestika Dashboard/Menu/Onboarding/Views/Register/Non Nasabah Verification/KetentuanRegisterNonNasabahView.swift
@@ -21,6 +21,8 @@ struct KetentuanRegisterNonNasabahView: View {
     @State var isActive : Bool = false
     @Binding var rootIsActive : Bool
     
+    @State var readFinished = false
+    
     // MARK: -MAIN CONTENT
     var body: some View {
         ZStack(alignment: .top) {
@@ -36,57 +38,58 @@ struct KetentuanRegisterNonNasabahView: View {
                 AppBarLogo(light: false, onCancel: {})
                 
                 VStack {
-                        ZStack {
-                            VStack {
-                                Color(hex: "#232175")
-                                    .frame(height: UIScreen.main.bounds.height*0.2)
-                                Color(hex: "#F6F8FB")
-                            }
+                    ZStack {
+                        VStack {
+                            Color(hex: "#232175")
+                                .frame(height: UIScreen.main.bounds.height*0.2)
+                            Color(hex: "#F6F8FB")
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            Image("ic_bells")
+                                .resizable()
+                                .frame(width: 90, height: 90)
+                                .padding(.top, 30)
                             
-                            VStack(alignment: .leading) {
-                                Image("ic_bells")
-                                    .resizable()
-                                    .frame(width: 90, height: 90)
-                                    .padding(.top, 30)
-                                
-                                Text("Sebelum Memulai..!!")
-                                    .font(.custom("Montserrat-Bold", size: 22))
-                                    .foregroundColor(Color(hex: "#232175"))
-                                    .padding(.vertical, 10)
-                                
-                                WebView(urlString: Bundle.main.url(forResource: "term", withExtension: "html")?.absoluteString)
-                                
-                                NavigationLink(
-                                    destination: FormPhoneVerificationRegisterNasabahView(rootIsActive: self.$rootIsActive, root2IsActive: self.$isActive).environmentObject(registerData)
-                                    ){
-                                    Text("Lanjut Membuat Rekening")
-                                        .foregroundColor(.white)
-                                        .font(.custom("Montserrat-SemiBold", size: 14))
-                                        .frame(maxWidth: .infinity, minHeight: 50, maxHeight: 50)
-                                }
-                                .isDetailLink(false)
-                                .background(Color(hex: "#2334D0"))
-                                .cornerRadius(12)
-                                .padding(.bottom, 5)
-                                .padding(.top, 10)
-                                
-                                Button(action : {
-                                    self.appState.moveToWelcomeView = true
-                                }) {
-                                    Text("Batal Mendaftar")
-                                        .foregroundColor(.gray)
-                                        .font(.custom("Montserrat-SemiBold", size: 14))
-                                        .frame(maxWidth: .infinity, minHeight: 50, maxHeight: 50)
-                                }
-                                .cornerRadius(12)
-                                .padding(.bottom, 20)
+                            Text("Sebelum Memulai..!!")
+                                .font(.custom("Montserrat-Bold", size: 22))
+                                .foregroundColor(Color(hex: "#232175"))
+                                .padding(.vertical, 10)
+                            
+                            WebView(readFinished: self.$readFinished, urlString: Bundle.main.url(forResource: "term", withExtension: "html")?.absoluteString)
+                            
+                            NavigationLink(
+                                destination: FormPhoneVerificationRegisterNasabahView(rootIsActive: self.$rootIsActive, root2IsActive: self.$isActive).environmentObject(registerData)
+                            ){
+                                Text("Lanjut Membuat Rekening")
+                                    .foregroundColor(.white)
+                                    .font(.custom("Montserrat-SemiBold", size: 14))
+                                    .frame(maxWidth: .infinity, minHeight: 50, maxHeight: 50)
                             }
-                            .padding(.horizontal, 30)
-                            .frame(width: UIScreen.main.bounds.width - 40)
-                            .background(Color.white)
-                            .cornerRadius(15)
-                            .shadow(color: Color(hex: "#3756DF").opacity(0.2), radius: 15, x: 0, y: 4)
-                                .padding(.vertical, 30)
+                            .isDetailLink(false)
+                            .background(self.readFinished ? Color(hex: "#2334D0") : Color(hex: "#FFFFFF"))
+                            .disabled(!self.readFinished)
+                            .cornerRadius(12)
+                            .padding(.bottom, 5)
+                            .padding(.top, 10)
+                            
+                            Button(action : {
+                                self.appState.moveToWelcomeView = true
+                            }) {
+                                Text("Batal Mendaftar")
+                                    .foregroundColor(.gray)
+                                    .font(.custom("Montserrat-SemiBold", size: 14))
+                                    .frame(maxWidth: .infinity, minHeight: 50, maxHeight: 50)
+                            }
+                            .cornerRadius(12)
+                            .padding(.bottom, 20)
+                        }
+                        .padding(.horizontal, 30)
+                        .frame(width: UIScreen.main.bounds.width - 40)
+                        .background(Color.white)
+                        .cornerRadius(15)
+                        .shadow(color: Color(hex: "#3756DF").opacity(0.2), radius: 15, x: 0, y: 4)
+                        .padding(.vertical, 30)
                     }
                 }
                 .padding(.vertical, 25)
