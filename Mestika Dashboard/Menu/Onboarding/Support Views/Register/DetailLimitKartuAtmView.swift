b58c88bb69913ce07c1213378534640c56fbc1ff
Abdul R. Arraisi <itsarraisi@gmail.com>
2021-01-11 14:25:23 +0700

    scroll view pilih jenis atm

--- Mestika Dashboard/Menu/Onboarding/Support Views/Register/DetailLimitKartuAtmView.swift
+++ Mestika Dashboard/Menu/Onboarding/Support Views/Register/DetailLimitKartuAtmView.swift
@@ -25,20 +25,22 @@ struct DetailLimitKartuAtmView: View {
             .padding()
             .padding(.top, 20)
             
-            RowLimitKartuAtmView(title: NSLocalizedString("Penarikan Harian", comment: ""), value: card.description.limitPenarikanHarian)
-            RowLimitKartuAtmView(title: NSLocalizedString("Transfer Antar Sesama Bank Mestika", comment: ""), value: card.description.limitTransferAntarSesama)
-            RowLimitKartuAtmView(title: NSLocalizedString("Transfer ke Bank Lain", comment: ""), value: card.description.limitTransferKeBankLain)
-            RowLimitKartuAtmView(title: NSLocalizedString("Penarikan Harian", comment: ""), value: card.description.limitPenarikanHarian)
-            RowLimitKartuAtmView(title: NSLocalizedString("Payment", comment: ""), value: card.description.limitPayment)
-            RowLimitKartuAtmView(title: NSLocalizedString("Purchase", comment: ""), value: card.description.limitPurchase)
+            ScrollView {
+                RowLimitKartuAtmView(title: NSLocalizedString("Penarikan Harian", comment: ""), value: card.description.limitPenarikanHarian)
+                RowLimitKartuAtmView(title: NSLocalizedString("Transfer Antar Sesama Bank Mestika", comment: ""), value: card.description.limitTransferAntarSesama)
+                RowLimitKartuAtmView(title: NSLocalizedString("Transfer ke Bank Lain", comment: ""), value: card.description.limitTransferKeBankLain)
+                RowLimitKartuAtmView(title: NSLocalizedString("Penarikan Harian", comment: ""), value: card.description.limitPenarikanHarian)
+                RowLimitKartuAtmView(title: NSLocalizedString("Payment", comment: ""), value: card.description.limitPayment)
+                RowLimitKartuAtmView(title: NSLocalizedString("Purchase", comment: ""), value: card.description.limitPurchase)
+            }
             
             NavigationLink(destination: FormPilihDesainATMView().environmentObject(atmData).environmentObject(registerData),
-                label: {
-                    Text(NSLocalizedString("PILIH KARTU ATM INI", comment: ""))
-                        .foregroundColor(.white)
-                        .font(.custom("Montserrat-SemiBold", size: 14))
-                        .frame(maxWidth: .infinity, maxHeight: 40)
-                })
+                           label: {
+                            Text(NSLocalizedString("PILIH KARTU ATM INI", comment: ""))
+                                .foregroundColor(.white)
+                                .font(.custom("Montserrat-SemiBold", size: 14))
+                                .frame(maxWidth: .infinity, maxHeight: 40)
+                           })
                 .frame(height: 50)
                 .background(Color(hex: "#2334D0"))
                 .cornerRadius(12)
