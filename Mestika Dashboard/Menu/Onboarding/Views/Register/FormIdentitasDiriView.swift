c5b22115fda77083c8c4bcf7765ac8443ea2a112
Abdul R. Arraisi <itsarraisi@gmail.com>
2021-01-13 11:12:06 +0700

    fixed issue scan ktp, failed retrieve image

--- Mestika Dashboard/Menu/Onboarding/Views/Register/FormIdentitasDiriView.swift
+++ Mestika Dashboard/Menu/Onboarding/Views/Register/FormIdentitasDiriView.swift
@@ -52,7 +52,7 @@ struct FormIdentitasDiriView: View {
      */
     @State private var shouldPresentScanner = false
     @State private var shouldPresentCamera = false
-    @State private var cameraFileName = ""
+    @State private var cameraFileName = "ktp"
     
     var body: some View {
         
@@ -276,9 +276,9 @@ struct FormIdentitasDiriView: View {
                             if matched.count != 0 {
                                 self.nik = matched[0]
                             }
-                            
-                            print("self.cameraFileName \(self.cameraFileName)")
                         }
+                        
+                        print("self.cameraFileName \(self.cameraFileName)")
                         
                         let scanResult = retrieveImage(forKey: self.cameraFileName)
                         if let image = scanResult {
@@ -319,7 +319,9 @@ struct FormIdentitasDiriView: View {
      Fungsi untuk ambil Gambar dari Local Storage
      */
     private func actionSelection(_ selection: String) {
+        
         self.cameraFileName = selection
+        
         switch selection {
         case "ktp":
             self.formSelfie = false
