ab02dcfe59204e96a865939d586654f6513b18cd
Prima Jatnika <primajatnika271995@gmail.com>
2021-01-19 13:54:20 +0700

    Roleback Image Jenis ATM , Image Design ATM, and Result ATM from Base64

--- Mestika Dashboard/Menu/Onboarding/Views/Register/FormDetailKartuATMView.swift
+++ Mestika Dashboard/Menu/Onboarding/Views/Register/FormDetailKartuATMView.swift
@@ -27,14 +27,14 @@ struct FormDetailKartuATMView: View {
     @State var isShowingAlert: Bool = false
     
     @State private var is_video_call = UserDefaults.standard.string(forKey: "register_nasabah_video_call")
-
+    
     var body: some View {
         ZStack(alignment: .top) {
-//            VStack {
-//                Color(hex: "#232175")
-//                    .frame(height: 100)
-//                    .edgesIgnoringSafeArea(.all)
-//            }
+            //            VStack {
+            //                Color(hex: "#232175")
+            //                    .frame(height: 100)
+            //                    .edgesIgnoringSafeArea(.all)
+            //            }
             
             VStack {
                 AppBarLogo(light: true, onCancel: {})
@@ -49,23 +49,28 @@ struct FormDetailKartuATMView: View {
                             Spacer()
                         }
                         .padding(.top, 10)
-    //                    Image("atm_bromo")
-    //                        .resizable()
-    //                        .aspectRatio(contentMode: .fit)
+                        //                    Image("atm_bromo")
+                        //                        .resizable()
+                        //                        .aspectRatio(contentMode: .fit)
                         
-                        WebImage(url: registerData.desainKartuATMImage)
-                            .onSuccess { image, data, cacheType in
-                                // Success
-                                // Note: Data exist only when queried from disk cache or network. Use `.queryMemoryData` if you really need data
-                            }
-                            .placeholder {
-                                Rectangle().foregroundColor(.gray).opacity(0.5)
-                            }
+                        //                        WebImage(url: registerData.desainKartuATMImage)
+                        //                            .onSuccess { image, data, cacheType in
+                        //                                // Success
+                        //                                // Note: Data exist only when queried from disk cache or network. Use `.queryMemoryData` if you really need data
+                        //                            }
+                        //                            .placeholder {
+                        //                                Rectangle().foregroundColor(.gray).opacity(0.5)
+                        //                            }
+                        //                            .resizable()
+                        //                            .indicator(.activity) // Activity Indicator
+                        //                            .transition(.fade(duration: 0.5)) // Fade Transition with duration
+                        //                            .scaledToFill()
+                        //                            .cornerRadius(10)
+                        
+                        Image(uiImage: registerData.desainKartuATMImage)
                             .resizable()
-                            .indicator(.activity) // Activity Indicator
-                            .transition(.fade(duration: 0.5)) // Fade Transition with duration
-                            .scaledToFill()
-                            .cornerRadius(10)
+                            .aspectRatio(contentMode: .fit)
+                            .background(Color.clear)
                         
                         HStack {
                             Text(NSLocalizedString("Selamat data kartu ATM baru Anda telah berhasil disimpan.", comment: ""))
@@ -95,51 +100,51 @@ struct FormDetailKartuATMView: View {
                                 .cornerRadius(10)
                             }
                             
-//                            if (is_video_call == "true" || registerData.isNasabahmestika == false) {
-//                                EmptyView()
-//                            } else {
-//                                Group {
-//                                    HStack {
-//                                        Text(NSLocalizedString("No. Kartu", comment: ""))
-//                                            .font(.custom("Montserrat-Regular", size: 12))
-//                                            .foregroundColor(Color(hex: "#707070"))
-//
-//                                        Spacer()
-//
-//                                        TextField(NSLocalizedString("No. Kartu", comment: ""), text: Binding.constant("")) { (isChanged) in
-//
-//                                        } onCommit: {
-//
-//                                        }
-//                                        .font(.custom("Montserrat-Regular", size: 12))
-//                                        .frame(width: 200, height: 36)
-//                                        .padding(.horizontal)
-//                                        .background(Color.gray.opacity(0.1))
-//                                        .cornerRadius(10)
-//                                        .disabled(true)
-//                                    }
-//
-//                                    HStack {
-//                                        Text("Expired")
-//                                            .font(.custom("Montserrat-Regular", size: 12))
-//                                            .foregroundColor(Color(hex: "#707070"))
-//
-//                                        Spacer()
-//
-//                                        TextField("Expired", text: Binding.constant("")) { (isChanged) in
-//
-//                                        } onCommit: {
-//
-//                                        }
-//                                        .font(.custom("Montserrat-Regular", size: 12))
-//                                        .frame(width: 200, height: 36)
-//                                        .padding(.horizontal)
-//                                        .background(Color.gray.opacity(0.1))
-//                                        .cornerRadius(10)
-//                                        .disabled(true)
-//                                    }
-//                                }
-//                            }
+                            //                            if (is_video_call == "true" || registerData.isNasabahmestika == false) {
+                            //                                EmptyView()
+                            //                            } else {
+                            //                                Group {
+                            //                                    HStack {
+                            //                                        Text(NSLocalizedString("No. Kartu", comment: ""))
+                            //                                            .font(.custom("Montserrat-Regular", size: 12))
+                            //                                            .foregroundColor(Color(hex: "#707070"))
+                            //
+                            //                                        Spacer()
+                            //
+                            //                                        TextField(NSLocalizedString("No. Kartu", comment: ""), text: Binding.constant("")) { (isChanged) in
+                            //
+                            //                                        } onCommit: {
+                            //
+                            //                                        }
+                            //                                        .font(.custom("Montserrat-Regular", size: 12))
+                            //                                        .frame(width: 200, height: 36)
+                            //                                        .padding(.horizontal)
+                            //                                        .background(Color.gray.opacity(0.1))
+                            //                                        .cornerRadius(10)
+                            //                                        .disabled(true)
+                            //                                    }
+                            //
+                            //                                    HStack {
+                            //                                        Text("Expired")
+                            //                                            .font(.custom("Montserrat-Regular", size: 12))
+                            //                                            .foregroundColor(Color(hex: "#707070"))
+                            //
+                            //                                        Spacer()
+                            //
+                            //                                        TextField("Expired", text: Binding.constant("")) { (isChanged) in
+                            //
+                            //                                        } onCommit: {
+                            //
+                            //                                        }
+                            //                                        .font(.custom("Montserrat-Regular", size: 12))
+                            //                                        .frame(width: 200, height: 36)
+                            //                                        .padding(.horizontal)
+                            //                                        .background(Color.gray.opacity(0.1))
+                            //                                        .cornerRadius(10)
+                            //                                        .disabled(true)
+                            //                                    }
+                            //                                }
+                            //                            }
                         }
                         
                         HStack {
@@ -166,7 +171,7 @@ struct FormDetailKartuATMView: View {
                     .padding(.vertical, 25)
                 }
             }
-        
+            
         }
         .edgesIgnoringSafeArea(.all)
         .navigationBarBackButtonHidden(true)
