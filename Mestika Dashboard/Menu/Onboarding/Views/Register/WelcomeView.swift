4e0439e67ad46fa76b5bae5581fa8d371ba4d976
Andri Ferinata <andri.ferinata@gmail.com>
2021-01-19 18:39:13 +0700

    Fix bypass screen on welcome view

--- Mestika Dashboard/Menu/Onboarding/Views/Register/WelcomeView.swift
+++ Mestika Dashboard/Menu/Onboarding/Views/Register/WelcomeView.swift
@@ -109,8 +109,8 @@ struct WelcomeView: View {
                         .disabled(isLoading)
                         
                         NavigationLink(destination:
-//                                        FirstLoginView().environmentObject(loginData),
-                                        FormDetailKartuATMView().environmentObject(AddProductATM()).environmentObject(RegistrasiModel()),
+                                        FirstLoginView().environmentObject(loginData),
+//                                        FormDetailKartuATMView().environmentObject(AddProductATM()).environmentObject(RegistrasiModel()),
                                        isActive: self.$isLoginViewActive){
                             Text(NSLocalizedString("Login", comment: ""))
                                 .foregroundColor(.white)
