9d0b06bf338697422a4cd30ac9666750b24bad33
Prima Jatnika <primajatnika271995@gmail.com>
2021-01-20 16:42:47 +0700

    FIxing deui Fixing deui, ulah diedit push aing

--- Mestika Dashboard/Menu/Onboarding/Views/Register/WelcomeView.swift
+++ Mestika Dashboard/Menu/Onboarding/Views/Register/WelcomeView.swift
@@ -109,7 +109,7 @@ struct WelcomeView: View {
                         .disabled(isLoading)
                         
                         NavigationLink(destination:
-                                        FirstLoginView().environmentObject(loginData),
+                                        PINView().environmentObject(registerData),
 //                                        FormPilihDesainATMView().environmentObject(AddProductATM()).environmentObject(RegistrasiModel()),
                                        isActive: self.$isLoginViewActive){
                             Text(NSLocalizedString("Login", comment: ""))
@@ -237,7 +237,11 @@ struct WelcomeView: View {
     func popupMenu() -> some View {
         switch modalSelection {
         case "CREATED" :
-            return AnyView(PopupCreated())
+            if (status_register_nasabah == "true") {
+                return AnyView(PopupKYCScheduled())
+            } else {
+                return AnyView(PopupCreated())
+            }
         case "KYC_SCHEDULED" :
             return AnyView(PopupKYCScheduled())
         case "KYC_WAITING" :
