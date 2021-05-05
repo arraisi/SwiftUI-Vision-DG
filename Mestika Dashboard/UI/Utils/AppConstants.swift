319261325cb876cb8d42adfde800674e78f09849
Prima Jatnika <primajatnika271995@gmail.com>
2021-05-05 13:54:31 +0700

    URL

--- Mestika Dashboard/UI/Utils/AppConstants.swift
+++ Mestika Dashboard/UI/Utils/AppConstants.swift
@@ -11,7 +11,7 @@ class AppConstants {
     
     
     #if DEBUG
-    var BASE_URL = "http://eagle.visiondg.xyz:8765/api/v1.0"
+    var BASE_URL = "http://eagle-dev.apps.visiondg.bankmestika.co.id/api/v1.0"
     #elseif DEBUGQA
     var BASE_URL = "http://eagle.visiondg.xyz:8765/api/v1.0"
     #elseif DEBUGMESTIKA
@@ -35,5 +35,5 @@ class AppConstants {
     
     var PUBLIC_KEY_RSA = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC7l8gErdf9mYzocGvR0uiNUjQrx9hf8lbIAsFlL0fZXEgZ8Ba+ZXMhbXW4rTgnFZx8Bo6wIXIFoNjqn5eFGjLvtKn5LX+Ul9k4j85pgsW40n29HNG3MAf4gSd+gtRD5a/OxzF9GCmipOAPRFiitazLzAwme9A9J2i/SyIO+VlTVQIDAQAB"
     
-    var BYPASS_OTP: Bool = false
+    var BYPASS_OTP: Bool = true
 }
